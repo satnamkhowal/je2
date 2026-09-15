54b5000d6f48fda1248e6434845401204e5b6016
Satnam Singh <singhkhowal@gmail.com>
2026-09-15 10:58:04 +0530

    Load responsive overrides after theme CSS

--- head.php
+++ head.php
@@ -67,6 +67,8 @@ if (isset($course) && is_array($course) && !empty($course['h1']) && !empty($cano
 <link rel="stylesheet" type="text/css" href="style.css"> <!-- This stylesheet dynamically changed from style.less -->
 <!-- responsive css -->
 <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+<!-- Jaipur Engineers device-specific fixes -->
+<link rel="stylesheet" type="text/css" href="assets/css/site-responsive-overrides.css">
 <!--[if lt IE 9]>
             <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
             <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
