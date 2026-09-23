47561afb2866142d7b8f1a719efffd66fa9d3c01
Satnam Singh <singhkhowal@gmail.com>
2026-09-23 11:51:26 +0530

    Add mail configuration sample

---
+++ config/mail.sample.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'enabled' => false,
+    'driver' => 'smtp',
+    'host' => 'smtp.example.com',
+    'port' => 587,
+    'encryption' => 'tls',
+    'username' => 'user@example.com',
+    'password' => 'change-me',
+    'from_email' => 'website@example.com',
+    'from_name' => 'Jaipur Engineers',
+    'notify_email' => 'leads@example.com',
+];
