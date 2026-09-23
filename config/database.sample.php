576dec1bb36529160e21fc96d7b22ba449ecf016
Satnam Singh <singhkhowal@gmail.com>
2026-09-23 11:55:40 +0530

    Document database port used by installer

--- config/database.sample.php
+++ config/database.sample.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'host' => 'localhost',
+    'port' => 3306,
     'database' => 'jaipur_engineers',
     'username' => 'database_user',
     'password' => 'database_password',
     'charset' => 'utf8mb4',
 ];
-?>
